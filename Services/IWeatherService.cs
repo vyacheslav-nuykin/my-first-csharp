8e87a36131555b2cdbada7b8f71796b0e3f222db
Vyacheslav Nuykin <nujkinvaceslav@gmail.com>
2026-09-13 15:07:06 +0100

    feat: define IWeatherService contract

---
+++ Services/IWeatherService.cs
@@ -0,0 +1,15 @@
+﻿using MyFirstCsharp.Models;
+
+namespace MyFirstCsharp.Services
+{
+    /// <summary>
+    /// Weather service contract.
+    /// </summary>
+    public interface IWeatherService
+    {
+        Task<WeatherResponse> GetCurrentWeatherAsync(
+            double latitude,
+            double longitude,
+            CancellationToken cancellationToken = default);
+    }
+}
